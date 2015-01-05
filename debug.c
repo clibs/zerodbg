c75771a97053df5af92eeff131f3c026195d3c4a
aameen951 <aameen951@gmail.com>
2015-01-05 17:04:24 +0300

    add debug_printf() and optimize the debug_set_log
    the debug_printf function will be used to avoid calling debug_set_log every time
    and remove the check FILE_LOG == NULL and make the check only 
    happens when setting the log file

--- debug.c
+++ debug.c
@@ -5,14 +5,26 @@
 
 #include "debug.h"
 
-FILE * LOG_FILE = NULL;
+FILE * LOG_FILE = stderr;
 
 void debug_set_log(FILE * log_file) {
-    LOG_FILE = log_file;
+    if(log_file != NULL)
+        LOG_FILE = log_file;
+    else
+        LOG_FILE = stderr;
 }
 
 FILE * debug_get_log() {
-    return LOG_FILE != NULL ? LOG_FILE : stderr;
+    return LOG_FILE;
+}
+
+void debug_printf(const char * format, ...)
+{
+    va_list args;
+
+    va_start(args, format);
+    vfprintf(LOG_FILE, format, args);
+    va_end(args);
 }
 
 void debug_fprintf(FILE * log_file, const char * format, ...) {
